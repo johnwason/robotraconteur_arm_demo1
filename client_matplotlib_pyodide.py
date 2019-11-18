86b267a5995e9bc2b3d1e5ef5deea018c5508c9d
hehonglu123 <44752310+hehonglu123@users.noreply.github.com>
2019-11-18 16:44:56 -0500

    Add files via upload

--- client_matplotlib_pyodide.py
+++ client_matplotlib_pyodide.py
@@ -196,12 +196,12 @@ async def client_matplotlib():
 		canvas = document.getElementById("image")
 		ctx = canvas.getContext("2d")
 
-		d=Uint8ClampedArray.new(40000)
+		d=bytes(40000)
 		for i in range(0,len(d),4):
-			d[i+0] = 255
-			d[i+1] = 0
-			d[i+2] = 0
-			d[i+3] = 255
+			d[i+0] = bytes([255])
+			d[i+1] = bytes([0])
+			d[i+2] = bytes([0])
+			d[i+3] = bytes([255])
 
 		image_data=ImageData.new(d,100,100)
 		print_div(image_data.data)
